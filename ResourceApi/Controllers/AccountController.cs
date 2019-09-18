f5abec253378925fa8740e7e24df0825b4c4ef63
Abror91 <a.kurbanov15@gmail.com>
2019-09-18 19:23:37 +0500

    Membership classes added

--- ResourceApi/Controllers/AccountController.cs
+++ ResourceApi/Controllers/AccountController.cs
@@ -12,6 +12,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json.Linq;
+using ResourceApi.Membership;
 using ResourceApi.Models;
 
 namespace ResourceApi.Controllers
@@ -36,21 +37,11 @@ namespace ResourceApi.Controllers
 
             using (var client = _clientFactory.CreateClient())
             {
-                client.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/x-www-form-urlencoded");
-                var tokenResponse = await client.RequestPasswordTokenAsync(new PasswordTokenRequest
-                {
-                    Address = "http://178.33.123.109:8080/auth/realms/dev/protocol/openid-connect/token",
-                    ClientId = "btc",
-                    ClientSecret = "381c1d29-2309-4529-a268-df162b0ec74c",
-                    UserName = model.Email,
-                    Password = model.Pasword
-                });
-                return Ok(tokenResponse.AccessToken);
+                var result = await LoginMembership.GetAccessToken(client, model);
+                return Ok(result);
             }
         }
-
-        //Authorize-> hamas tekshrad,siza kkli schema k,basic template kkmas bizga detalni xatozi yozin
-        //Gitignore oqshin ahjma foile trash,2-3 line bn ish bitmed
+        
 
         [HttpGet]
         [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
@@ -59,15 +50,13 @@ namespace ResourceApi.Controllers
             var tokenValue = HttpContext.Request.Headers.SingleOrDefault(s => s.Key == "Authorization");
             using (var client = _clientFactory.CreateClient())
             {
-                client.DefaultRequestHeaders.Authorization = AuthenticationHeaderValue.Parse(tokenValue.Value);
-                var userInfo = await client.GetAsync("http://178.33.123.109:8080/auth/realms/dev/protocol/openid-connect/userinfo");
-                var user = await userInfo.Content.ReadAsAsync<UserModel>();
+                var user = await UserInfoMembership.GetUserInfo(client, tokenValue);
                 return Ok(user);
             };
         }
 
         [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-        public HttpResponseMessage Logout()
+        public async Task<HttpResponseMessage> Logout()
         {
             var result = new HttpResponseMessage();
             using (var reader = new StreamReader(Request.Body))
@@ -78,15 +67,7 @@ namespace ResourceApi.Controllers
                 using (var client = _clientFactory.CreateClient())
                 {
                     var tokenValue = HttpContext.Request.Headers.SingleOrDefault(s => s.Key == "Authorization");
-
-                    client.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/x-www-form-urlencoded");
-                    client.DefaultRequestHeaders.Authorization = AuthenticationHeaderValue.Parse(tokenValue.Value);
-                    HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "http://178.33.123.109:8080/auth/realms/dev/protocol/openid-connect/logout");
-                    request.Content = new StringContent(refreshToken);
-                    client.SendAsync(request).ContinueWith(response =>
-                    {
-                        result = response.Result;
-                    });
+                    result = await LogoutMembership.Logout(client, tokenValue, refreshToken);
                 }
             }
             return result;
---
+++ ResourceApi/Membership/UserInfoMembership.cs
@@ -0,0 +1,21 @@
+﻿using ResourceApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace ResourceApi.Membership
+{
+    public class UserInfoMembership
+    {
+        public static async Task<UserModel> GetUserInfo(HttpClient client, dynamic token)
+        {
+            client.DefaultRequestHeaders.Authorization = AuthenticationHeaderValue.Parse(token.Value);
+            var userInfo = await client.GetAsync("http://178.33.123.109:8080/auth/realms/dev/protocol/openid-connect/userinfo");
+            var user = await userInfo.Content.ReadAsAsync<UserModel>();
+            return user;
+        }
+    }
+}
